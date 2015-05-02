add5e993a9e4d3da54e20ecc9f018254b6718b61
Sungtae Ryu <sungtae.ryu@goorm.io>
2015-05-02 03:48:07 +0000

    update collaboration.panel
    git-svn-id: http://devmngt.goorm.io/svn/goorm_test/trunk@9 d7237ca1-ace6-4659-bc5c-c1c056bdef01

--- collaboration/collaboration.share.panel.js
+++ collaboration/collaboration.share.panel.js
@@ -29,7 +29,7 @@ module.exports = {
       .click('#main-menu-project a[action=share_project]')
       .waitForElementVisible('#dlg_project_share', 3000)
       .pause(2000)
-      .assert.attributeEquals('#project_share_select_list :checked', 'localization_key', 'select_project')      
+      .verify.attributeEquals('#project_share_select_list :checked', 'localization_key', 'select_project')      
       .waitForElementPresent('.modal-backdrop.fade.in', 10000)
       .click('#g_prjs_btn_cancel')
       .waitForElementNotPresent('.modal-backdrop', 10000)
@@ -41,22 +41,23 @@ module.exports = {
       .pause(1000)
       .click('button#project_selectbox')
       .waitForElementPresent('#project_selector > div.open', 3000)
+      .verify.elementPresent('#shared_header + li')
       .getAttribute('#shared_header + li', 'project_path', function (result) {
         var project_path = result.value;
         var project_name = project_path.split("_").slice(2).join("_");
         this.click('#shared_header + li')
-        .pause(2000)
-        .waitForElementNotVisible('#dlg_loading_bar', 10000)
-        .verify.containsText('#selected_project_name', project_name)
-        .click('#main-menu-project > a')
-        .waitForElementPresent('#main-menu-project.open', 2000)
-        .click('#main-menu-project a[action=share_project]')
-        .waitForElementVisible('#dlg_project_share', 3000)
-        .pause(2000)
-        .assert.attributeEquals('#project_share_select_list :checked', 'localization_key', 'select_project')
-        .waitForElementPresent('.modal-backdrop.fade.in', 10000)
-        .click('#g_prjs_btn_cancel')
-        .waitForElementNotPresent('.modal-backdrop', 10000);
+          .pause(2000)
+          .waitForElementNotVisible('#dlg_loading_bar', 10000)
+          .verify.containsText('#selected_project_name', project_name)
+          .click('#main-menu-project > a')
+          .waitForElementPresent('#main-menu-project.open', 2000)
+          .click('#main-menu-project a[action=share_project]')
+          .waitForElementVisible('#dlg_project_share', 3000)
+          .pause(2000)
+          .verify.attributeEquals('#project_share_select_list :checked', 'localization_key', 'select_project')
+          .waitForElementPresent('.modal-backdrop.fade.in', 10000)
+          .click('#g_prjs_btn_cancel')
+          .waitForElementNotPresent('.modal-backdrop', 10000);
       });
   },
   'open_owned_project_and_share_panel' : function (browser) {
@@ -70,20 +71,50 @@ module.exports = {
         var project_path = result.value;
         var project_name = project_path.split("_").slice(2).join("_");
         this.click('#my_projects_header + li')
-        .pause(2000)
-        .waitForElementNotVisible('#dlg_loading_bar', 10000)
-        .verify.containsText('#selected_project_name', project_name)
-        .click('#main-menu-project > a')
-        .waitForElementPresent('#main-menu-project.open', 2000)
-        .click('#main-menu-project a[action=share_project]')
-        .waitForElementVisible('#dlg_project_share', 3000)
-        .pause(2000)
-        .assert.containsText('#project_share_select_list :checked', project_path)
+          .pause(2000)
+          .waitForElementNotVisible('#dlg_loading_bar', 10000)
+          .verify.containsText('#selected_project_name', project_name)
+          .click('#main-menu-project > a')
+          .waitForElementPresent('#main-menu-project.open', 2000)
+          .click('#main-menu-project a[action=share_project]')
+          .waitForElementVisible('#dlg_project_share', 3000)
+          .pause(2000)
+          .verify.containsText('#project_share_select_list :checked', project_path)
       })
-      .getAttribute('#user_search_input', 'placeholder', function (result) {
-        var result = result.value;
-        this.assert.equal(true, /email|이메일/.test(result), 'Element <#user_search_input> has placeholder contain "email" or "이메일"');
-        this.end();
+    },
+  'check_placehold' : function (browser) {
+      browser
+        .getAttribute('#user_search_input', 'placeholder', function (result) {
+          var result = result.value;
+          this.verify.equal(true, /email|이메일/.test(result), 'Element <#user_search_input> has placeholder contain "email" or "이메일"');
+        })
+  },
+  'find_exist_user_with_email' : function (browser) {
+    browser
+      .setValue('#user_search_input', 'attainer')
+      .waitForElementVisible('#user_search_result_container > ul', 3000, false, function (result){
+        if (result.value) {
+          this
+            .verify.containsText('#user_search_result_container > ul a:first-child', 'attainer')
+            .clearValue('#user_search_input')
+            .setValue('#user_search_input', 'nys')
+            .pause(2000)
+            .verify.containsText('#user_search_result_container > ul a:first-child', 'nys')
+            
+        }
+        this.clearValue('#user_search_input')
       })
   },
+  'find_exist_user_with_name' : function (browser) {
+    browser
+      .setValue('#user_search_input', '테스트')
+      .pause(2000)
+      .waitForElementVisible('#user_search_result_container > ul', 3000, false)
+      .verify.containsText('#user_search_result_container > ul a:first-child', '테스트')
+      .clearValue('#user_search_input')
+      .end()
+
+  }
+
+
 }
