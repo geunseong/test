50803bee509bf85f33800897ae6e8a59528ac7d2
Kyeongwook Jeong <kyeongwook.jeong@goorm.io>
2015-05-02 15:21:15 +0000

    Member - logout with a file not saved
    git-svn-id: http://devmngt.goorm.io/svn/goorm_test/trunk@15 d7237ca1-ace6-4659-bc5c-c1c056bdef01

--- member/member.logout.no_survey.js
+++ member/member.logout.no_survey.js
@@ -15,51 +15,49 @@ module.exports = {
       .waitForElementVisible('#workspace', 120000)
       .verify.urlEquals('http://ide.goorm.io/');
   },
-  // 'open_file' : function (browser) {
-  // 	browser
-  // 		.waitForElementPresent('li.me img.user_profile_image', 20000)
-		// 	.waitForElementNotVisible('#dlg_loading_bar', 10000)
-		// 	.pause(1000)
-		// 	.click('button#project_selectbox')
-		// 	.waitForElementPresent('#project_selector > div.open', 3000)
-		// 	.getAttribute('#my_projects_header + li', 'project_path', function (result) {
-		// 		var project_path = result.value;
-		// 		var project_name = project_path.split("_").slice(2).join("_");
-		// 		this.click('#my_projects_header + li')
-		// 			.waitForElementNotVisible('#dlg_loading_bar', 10000)
-		// 			.verify.containsText('#selected_project_name', project_name)
-		// 	})
-		// 	.click('#main-menu-file > a')
-		// 	.waitForElementPresent('#main-menu-file.open', 2000)
-		// 	.click('#main-menu-file a[action=open_file]')
-		// 	.waitForElementVisible('#dlg_open_file')
-		// 	.waitForElementPresent('#file_open_files .file_item')
-		// 	.click('#file_open_files .file_item')
-		// 	.pause(500)
-		// 	.getValue('#file_open_target_name', function (result) {
-		// 		this.verify.equal(true, /^[A-Za-z]/.test(result.value))
-		// 	})
-		// 	.click('#g_of_btn_ok')
-		// 	.waitForElementNotVisible('#dlg_open_file', 1000)
-		// 	.waitForElementPresent('.g_windows_tab_li .active', 2000)
-  // }
-  'open_logout_confirm' : function (browser) {
+  'open_file' : function (browser) {
   	browser
   		.waitForElementPresent('li.me img.user_profile_image', 20000)
 			.waitForElementNotVisible('#dlg_loading_bar', 10000)
 			.pause(1000)
+			.click('button#project_selectbox')
+			.waitForElementPresent('#project_selector > div.open', 3000)
+			.getAttribute('#my_projects_header + li', 'project_path', function (result) {
+				var project_path = result.value;
+				var project_name = project_path.split("_").slice(2).join("_");
+				this.click('#my_projects_header + li')
+					.waitForElementNotVisible('#dlg_loading_bar', 10000)
+					.verify.containsText('#selected_project_name', project_name)
+			})
+			.pause(3000)
+			.waitForElementNotVisible('#dlg_loading_bar', 10000)
+			.click('#main-menu-file > a')
+			.waitForElementPresent('#main-menu-file.open', 2000)
+			.click('#main-menu-file a[action=open_file]')
+			.waitForElementVisible('#dlg_open_file', 1000)
+			.waitForElementPresent('#file_open_files .file_item', 10000)
+			.click('#file_open_files .file_item')
+			.pause(500)
+			.getValue('#file_open_target_name', function (result) {
+				this.verify.equal(true, /^[A-Za-z]/.test(result.value))
+			})
+			.click('#g_of_btn_ok')
+			.waitForElementNotVisible('#dlg_open_file', 1000)
+			.waitForElementPresent('li.g_windows_tab_li', 2000)
+			.execute(function() {
+				core.module.layout.workspace.window_manager.window[0].editor.editor.setValue('@@@');
+			}, [])
+			.verify.visible('span.title_option')
+  },
+  'try_logout' : function (browser) {
+  	browser
 			.click('#user-email-container')
 			.waitForElementPresent('#fat-menu.open', 1000)
 			.assert.cssClassPresent('#fat-menu', 'open')
 			.click('a[action="account_logout"]')
-			.waitForElementVisible('#dlg_confirmation', 1000)
-			.assert.visible('#confirmation_content_container')
-  },
-  'logout_yes' : function (browser) {
-  	browser
-  		.click('#g_cfrm_btn_yes')
-  		.waitForElementVisible('#dlg_survey', 1000)
-  		.assert.visible('#dlg_survey')
+			.waitForElementVisible('#dlg_confirmation_save', 1000)
+			.assert.visible('#g_cfrm_s_btn_yes')
+			.click('#g_cfrm_s_btn_no')
   },
   'survey_no' : function (browser) {
   	browser
