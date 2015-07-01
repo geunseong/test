b33f3e7505d3e4cbdde78b6cb28f121d2bd0a89c
Sungtae Ryu <sungtae.ryu@goorm.io>
2015-07-01 07:03:01 +0000

    add nodejs linter testcasse
    git-svn-id: http://devmngt.goorm.io/svn/goorm_test/trunk@62 d7237ca1-ace6-4659-bc5c-c1c056bdef01

--- testcase/plugins/nodejs/nodejs.default_project.lint.js
+++ testcase/plugins/nodejs/nodejs.default_project.lint.js
@@ -1,6 +1,7 @@
-var project_name = 'nodejs_test224';
+var project_name = 'nodejs_test12';
 var plugin = 'nodejs';
 var detail_type = 'default';
+var file_name = 'main.js';
 
 module.exports = {
   'goorm_login' : function (browser) {
@@ -11,6 +12,7 @@ module.exports = {
   'create_default_project' : function (browser) {
     browser
       .new_project(plugin, detail_type, project_name)
+      .pause(2000)
   },
   'make_error_and_warning_file' : function (browser) {
     browser
@@ -35,14 +37,50 @@ module.exports = {
       .run_project('menu')
       .pause(3000)
   },
-  'check_output' : function (browser) {
+  'check_output_exist' : function (browser) {
     browser
-      .click('#gLayoutOutput_nodejs')
-      .waitForElementVisible('#output_tab_nodejs tbody tr', 20000, false)
-      .verify.containsText('#output_tab_nodejs tbody', 'Error')
+      .click('#gLayoutOutput_' + plugin)
+      .waitForElementVisible('#output_tab_' + plugin + ' tbody tr', 20000, false)
+      .verify.containsText('#output_tab_' + plugin + ' tbody', 'Error')
+      .verify.elementPresent('.fa-times-circle'); //error
+  },
+  'division_error_and_warning' : function (browser) {
+    browser
       .verify.containsText('#output_tab_nodejs tbody', 'Warning')
       .verify.elementPresent('.fa-exclamation-triangle') //warning
-      .verify.elementPresent('.fa-times-circle') //error
+  },
+  'check_error_box_and_output' : function (browser) {
+    browser
+      .verify.containsText('#output_tab_' + plugin + ' tbody', 'Expected')
+      .moveToElement('.fa-times-circle', 3, 3)
+      .verify.containsText('.error_message_box', 'Expected');
+  },
+  'check_line' : function (browser) {
+    browser
+      .pause(1000)
+      .click('#output_tab_' + plugin + ' tbody tr:nth-child(2) td:nth-child(3)')
+      .pause(3000)
+      .getText('#output_tab_' + plugin + ' tbody tr:nth-child(2) td:nth-child(3)', function(result) {
+        var line = result.value;
+        browser.getText('.CodeMirror-activeline .CodeMirror-linenumber', function(result) {
+          browser.verify.equal(line, result.value);
+        })
+      });
+  },
+  'check_line2' : function (browser) {
+    browser
+      .click('.close.tab_close_button')
+      .waitForElementNotPresent('[filename="' + file_name + '"]', 10000, false)
+      .click('#output_tab_' + plugin + ' tbody tr:nth-child(2) td:nth-child(3)')
+      .waitForElementPresent('[filename="' + file_name + '"]', 10000, false)
+      .verify.elementPresent('[filename="' + file_name + '"]')
+      .pause(2000)
+      .getText('#output_tab_' + plugin + ' tbody tr:nth-child(2) td:nth-child(3)', function(result) {
+        var line = result.value;
+        browser.getText('.CodeMirror-activeline .CodeMirror-linenumber', function(result) {
+          browser.verify.equal(line, result.value);
+        })
+      });
   },
   'delete_project' : function (browser) {
     browser
