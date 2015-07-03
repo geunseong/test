02455eb1691e0787ffe540a910ec2452c5056b04
Kyeongwook Jeong <kyeongwook.jeong@goorm.io>
2015-07-03 02:54:29 +0000

    Basic - layout - left
    git-svn-id: http://devmngt.goorm.io/svn/goorm_test/trunk@84 d7237ca1-ace6-4659-bc5c-c1c056bdef01

--- testcase/basic/basic.layout.js
+++ testcase/basic/basic.layout.js
@@ -3,36 +3,36 @@ module.exports = {
         var data = browser.globals;
 		browser.run_ide(data.username, data.password);
     },
-    'toggle_top': function(browser) {
-        browser
-            .pause(2000)
-            .verify.visible('#goorm-mainmenu')
-            .verify.visible('#goorm_main_toolbar')
-            .click('div.ui-layout-toggler-north')
-            .pause(2000)
-            .verify.hidden('#goorm_main_toolbar')
-            .click('div.ui-layout-toggler-north')
-            .pause(2000)
-            .verify.hidden('#goorm_top')
-            .click('div.ui-layout-toggler-north')
-            .pause(2000)
-            .verify.visible('#goorm-mainmenu')
-            .verify.visible('#goorm_main_toolbar')
-    },
-    'show_more_toolbar': function(browser) {
-        browser
-            .isVisible('#main_debug_toolbar', function(result) {
-                if (result.value == true) {
-                    this
-                        .resizeWindow(900, 700)
-                        .pause(3000)
-                        .verify.visible('#toolbar_more_button')
-                        .maximizeWindow()
-                        .pause(3000)
-                        .verify.hidden('#toolbar_more_button');
-                }
-            });
-    },
+    // 'toggle_top': function(browser) {
+    //     browser
+    //         .pause(2000)
+    //         .verify.visible('#goorm-mainmenu')
+    //         .verify.visible('#goorm_main_toolbar')
+    //         .click('div.ui-layout-toggler-north')
+    //         .pause(2000)
+    //         .verify.hidden('#goorm_main_toolbar')
+    //         .click('div.ui-layout-toggler-north')
+    //         .pause(2000)
+    //         .verify.hidden('#goorm_top')
+    //         .click('div.ui-layout-toggler-north')
+    //         .pause(2000)
+    //         .verify.visible('#goorm-mainmenu')
+    //         .verify.visible('#goorm_main_toolbar')
+    // },
+    // 'show_more_toolbar': function(browser) {
+    //     browser
+    //         .isVisible('#main_debug_toolbar', function(result) {
+    //             if (result.value == true) {
+    //                 this
+    //                     .resizeWindow(900, 700)
+    //                     .pause(3000)
+    //                     .verify.visible('#toolbar_more_button')
+    //                     .maximizeWindow()
+    //                     .pause(3000)
+    //                     .verify.hidden('#toolbar_more_button');
+    //             }
+    //         });
+    // },
     'window_perspectives_left': function(browser) {
         browser
             .verify.visible('#goorm_left')
@@ -48,6 +48,21 @@ module.exports = {
             .pause(2000)
             .verify.visible('#goorm_left')
     },
+    'window_perspectives_right': function(browser) {
+        browser
+            .verify.visible('#goorm_inner_layout_right')
+            .click('#main-menu-window')
+            .waitForElementPresent('#main-menu-window.open', 2000)
+            .moveToElement('#parent_perspectives_menu', 50, 10)
+            .waitForElementVisible('#child_perspectives_menu', 2000)
+            .click('#child_perspectives_menu a[action="right_layout_toggle"]')
+            .pause(2000)
+            .verify.hidden('#goorm_inner_layout_right')
+            .keys([browser.Keys.ALT, browser.Keys.SHIFT, 'r'])
+            .keys(browser.Keys.NULL)
+            .pause(2000)
+            .verify.visible('#goorm_inner_layout_right')
+    }
     'goorm_end': function(browser) {
         browser.end();
     }
