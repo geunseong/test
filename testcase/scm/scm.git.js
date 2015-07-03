93ee19d4f0a41c93005d314aa4669c73573643ba
Jeongmin Im <jeongmin.im@goorm.io>
2015-07-03 10:21:16 +0000

    SCM testcase update
    git-svn-id: http://devmngt.goorm.io/svn/goorm_test/trunk@106 d7237ca1-ace6-4659-bc5c-c1c056bdef01

--- testcase/scm/scm.git.js
+++ testcase/scm/scm.git.js
@@ -2,23 +2,23 @@ module.exports = {
   'goorm_login': function(browser) {
     var data = browser.globals;
 
-    // browser
-    //   .run_ide(data.username, data.password)
-    //   .waitForElementVisible('#workspace', 200000)
-    //   .waitForElementPresent('.user_profile_image', 10000)
-    //   .pause(2000)
-    //   .waitForElementNotVisible('#dlg_loading_bar', 100000)
-
     browser
-      .url(data.urls.local)
-      .waitForElementVisible('#goorm_id', 100000)
-      .setValue('#goorm_id', data.username)
-      .setValue('#goorm_pw', data.password)
-      .click('#goorm_login_button')
+      .run_ide(data.username, data.password)
       .waitForElementVisible('#workspace', 200000)
       .waitForElementPresent('.user_profile_image', 10000)
       .pause(2000)
-      .waitForElementNotVisible('#dlg_loading_bar', 10000)
+      .waitForElementNotVisible('#dlg_loading_bar', 100000)
+
+    // browser
+    //   .url(data.urls.local)
+    //   .waitForElementVisible('#goorm_id', 100000)
+    //   .setValue('#goorm_id', data.username)
+    //   .setValue('#goorm_pw', data.password)
+    //   .click('#goorm_login_button')
+    //   .waitForElementVisible('#workspace', 200000)
+    //   .waitForElementPresent('.user_profile_image', 10000)
+    //   .pause(2000)
+    //   .waitForElementNotVisible('#dlg_loading_bar', 10000)
   },
   'scm_validation': function(browser) {
     var url = 'https://github.com/openstack/horizon.git';
