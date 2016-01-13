b67383ce2d8ea4b19a4b81c020f7677af7a80a1c
geunseong <geunseong.lee@goorm.io>
2016-01-13 14:04:59 +0900

    test

--- nightwatch2/commands/move_to_project.js
+++
@@ -1,9 +0,0 @@
-exports.command = function(project_name) {
-	this
-		.click('#project_selectbox')
-		.waitForElementPresent('#project_selector .open', 2000)
-		.click('[project_path$=' + project_name + ']')
-		.pause(2000)
-		.waitForElementNotVisible('#dlg_loading_bar', 10000)
-	return this;
-};
--- nightwatch2/commands/replace_input.js
+++
@@ -1,5 +0,0 @@
-exports.command = function(cssSelector, newValue){
-  this.clearValue(cssSelector);
-  this.setValue(cssSelector, newValue);
-  return this;
-};
